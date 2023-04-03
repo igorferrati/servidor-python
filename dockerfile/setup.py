2e16684f738cf4722c350ae55db8f2434276d747
igorferrati <ferrati.igor@gmail.com>
2023-04-03 18:59:14 -0300

    run wsgi, kubectl

--- dockerfile/setup.py
+++ dockerfile/setup.py
@@ -1,5 +1,5 @@
 import subprocess as sub
 
-sub.run(["gunicorn", "-b", "0.0.0.0:8000", "wsgi:app"])
+sub.run(["gunicorn", "-b", "0.0.0.0:8000", "app.wsgi:app"])
 
 sub.run(["nginx", "-g", "daemon off;"])
--- dockerfile/wsgi.py
+++ dockerfile/wsgi.py
@@ -17,8 +17,9 @@ def app(environ, start_response):
     status = '200 OK'
     response_headers = [
         ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(data)))
+        ('Content-Length', str(len(data))),
+        ('Refresh', '5')
     ]
     start_response(status, response_headers)
-    return iter([data.decode()])
+    return iter([data])
 
